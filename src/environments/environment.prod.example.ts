991040087e7aadf1bda93a5985ff5f333e6d1e86
Mateo Sosa <mateesosar@gmail.com>
2026-05-26 20:25:06 -0300

    environment con rutas

--- src/environments/environment.prod.example.ts
+++ src/environments/environment.prod.example.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production:true,
-    apiUrl: '',
-    authApiUrl: '' ,
-    serverIp: '',
+    apiUrl: 'http://localhost:8001/',
+    authApiUrl: 'http://localhost:8000/' ,
+    serverIp: 'http://localhost:',
     clientId: '',
     clientSecret: ''
 };
