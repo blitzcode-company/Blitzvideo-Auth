2ada9d6954bdb2f5cc28f69488b2120abdc3f6b0
Mateo Sosa <mateesosar@gmail.com>
2026-05-26 20:14:10 -0300

    clientid y clientsecret a variables de entorno

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production:true,
-    apiUrl: 'http://localhost:8001/',
-    authApiUrl: 'http://localhost:8000/',
-    serverIp: 'http://localhost:',
-    clientId: '1',
-    clientSecret: 'bl7jx7EP0fzG0rB4h85TLAmSb3HpKcqJAUJfQlLF'
-};
